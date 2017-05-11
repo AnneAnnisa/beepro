3962404928ae9df462e7655ae0cca66c4e78d902
vnsnsia <vinsensiasip@gmail.com>
2017-05-11 12:49:42 +0700

    database brand, product, kategori done

---
+++ database/migrations/2017_05_11_052200_kategoriMigrate.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class KategoriMigrate extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+
+        Schema::create('kategori',function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('nama_kategori');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::drop('kategori');
+    }
+}
