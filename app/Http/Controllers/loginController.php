7f6778fe8613c952cf6d4ecf32bf8af2e0e4ee06
AnneAnnisa <anne14@mhs.if.its.ac.id>
2017-05-28 20:40:28 +0700

    fix all

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -14,42 +14,16 @@ class loginController extends Controller
    
     public function authenticate(Request $req)
     {
-    	// echo $req->input('email');
-    	// echo bcrypt($req->input('password'));
-        //if (Auth::attempt(['email' => $req->input('email'), 'password' => $req->input('password')])) {
-
-            // Authentication passed...
-            //echo 'asd';
-            // return redirect()->intended('home');
-        //}
-       //return redirect()->intended('home');
-        //echo 'salah';
-        
         $email = $req->email;
         $user = User::where('email', $req->email)->first();
         if($user)
         {
         	if(Hash::check($req->password, $user->password))
         	{
-        		        	//echo "masuk";
         		$datauser = array('email' => $user['email'], 'nama' => $user['nama'], 'id' => $user['id'] );
-                // $ID = 'id'=> $user['id'];
-        		// echo $user['email'];
-        		//dd($datauser);
-        		//$req->session()->put('user',$datauser);
         		session(['user' => $datauser]);
-                //session(['id' => $ID]);
-        		//dd($datauser);
-        		//dd(session('user')["email"]);
         		return redirect('/home');
-        
-
-        		//return redirect::to('home');
            	}
-           //	$string = bcrypt($req->password);
-        // 	echo $string;
-        // echo 'gamasuk';
-        // dd($user);
         }
 
     }
--- beepro.sql
+++ beepro.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: May 16, 2017 at 10:13 AM
+-- Generation Time: May 19, 2017 at 06:34 AM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 5.6.24
 
@@ -36,8 +36,24 @@ CREATE TABLE `brand` (
 --
 
 INSERT INTO `brand` (`id`, `nama_brand`) VALUES
-(1, 'NYX'),
-(2, 'THE FACE SHOP');
+(1, 'THE BODY SHOP'),
+(2, 'NYKAA'),
+(3, 'LAKME'),
+(4, 'KIEHL''S'),
+(5, 'BIODERMA'),
+(6, 'L''OREAL PARIS'),
+(7, 'KYLIE'),
+(8, 'MARC JACOBS'),
+(9, 'THEBALM'),
+(10, 'KYLIE KIT'),
+(11, 'ORIFLAME'),
+(12, 'NYX'),
+(13, 'COLORBAR'),
+(14, 'M.A.C'),
+(15, 'ETUDE'),
+(16, 'INNISFREE'),
+(17, 'MAYBELINNE NEW YORK'),
+(18, 'THE FACE SHOP');
 
 -- --------------------------------------------------------
 
@@ -58,12 +74,12 @@ CREATE TABLE `foto` (
 --
 
 INSERT INTO `foto` (`id`, `review_id`, `path`, `created_at`, `updated_at`) VALUES
-(1, 1, '/review/1.jpg', NULL, NULL),
-(2, 1, '/review/2.jpg', NULL, NULL),
-(3, 2, '/review/3.jpg', NULL, NULL),
-(4, 3, '/review/4.jpg', NULL, NULL),
-(5, 4, '/review/5.jpg', NULL, NULL),
-(6, 5, '/review/p1.jpg', NULL, NULL);
+(1, 1, '/review/2.jpg', NULL, NULL),
+(2, 2, '/review/1.jpg', NULL, NULL),
+(3, 3, '/review/3.jpg', NULL, NULL),
+(4, 4, '/review/5.jpg', NULL, NULL),
+(5, 5, '/review/p1.jpg', NULL, NULL),
+(23, 16, '/review/123.jpg', '2017-05-17 20:36:04', '2017-05-17 20:36:04');
 
 -- --------------------------------------------------------
 
@@ -83,8 +99,13 @@ CREATE TABLE `hashtag` (
 --
 
 INSERT INTO `hashtag` (`id`, `nama_hashtag`, `created_at`, `updated_at`) VALUES
-(1, '#kylielipkit', NULL, NULL),
-(2, '#powder', NULL, NULL);
+(1, '#kylie', '2017-05-16 23:38:25', '2017-05-16 23:38:25'),
+(2, '#lipkit', '2017-05-16 23:38:25', '2017-05-16 23:38:25'),
+(3, '#etude', '2017-05-16 23:38:25', '2017-05-16 23:38:25'),
+(4, '#lipgloss', '2017-05-16 23:38:26', '2017-05-16 23:38:26'),
+(5, '#viva', '2017-05-16 23:38:26', '2017-05-16 23:38:26'),
+(28, '#fefe', '2017-05-17 20:34:29', '2017-05-17 20:34:29'),
+(29, '#sdas', '2017-05-17 20:36:04', '2017-05-17 20:36:04');
 
 -- --------------------------------------------------------
 
@@ -103,7 +124,75 @@ CREATE TABLE `kategori` (
 
 INSERT INTO `kategori` (`id`, `nama_kategori`) VALUES
 (1, 'Face'),
-(2, 'Makeup');
+(2, 'Lips'),
+(3, 'Eyes'),
+(4, 'Make Up Kits'),
+(5, 'Cleanser'),
+(6, 'Moisturizer'),
+(7, 'Lip Care'),
+(8, 'Bath'),
+(9, 'Body'),
+(10, 'Hands & Feet'),
+(11, 'Face'),
+(12, 'Lips'),
+(13, 'Eyes'),
+(14, 'Make Up Kits'),
+(15, 'Cleanser'),
+(16, 'Moisturizer'),
+(17, 'Lip Care'),
+(18, 'Bath'),
+(19, 'Body'),
+(20, 'Hands & Feet'),
+(21, 'Face'),
+(22, 'Lips'),
+(23, 'Eyes'),
+(24, 'Make Up Kits'),
+(25, 'Cleanser'),
+(26, 'Moisturizer'),
+(27, 'Lip Care'),
+(28, 'Bath'),
+(29, 'Body'),
+(30, 'Hands & Feet'),
+(31, 'Face'),
+(32, 'Lips'),
+(33, 'Eyes'),
+(34, 'Make Up Kits'),
+(35, 'Cleanser'),
+(36, 'Moisturizer'),
+(37, 'Lip Care'),
+(38, 'Bath'),
+(39, 'Body'),
+(40, 'Hands & Feet'),
+(41, 'Face'),
+(42, 'Lips'),
+(43, 'Eyes'),
+(44, 'Make Up Kits'),
+(45, 'Cleanser'),
+(46, 'Moisturizer'),
+(47, 'Lip Care'),
+(48, 'Bath'),
+(49, 'Body'),
+(50, 'Hands & Feet'),
+(51, 'Face'),
+(52, 'Lips'),
+(53, 'Eyes'),
+(54, 'Make Up Kits'),
+(55, 'Cleanser'),
+(56, 'Moisturizer'),
+(57, 'Lip Care'),
+(58, 'Bath'),
+(59, 'Body'),
+(60, 'Hands & Feet'),
+(61, 'Face'),
+(62, 'Lips'),
+(63, 'Eyes'),
+(64, 'Make Up Kits'),
+(65, 'Cleanser'),
+(66, 'Moisturizer'),
+(67, 'Lip Care'),
+(68, 'Bath'),
+(69, 'Body'),
+(70, 'Hands & Feet');
 
 -- --------------------------------------------------------
 
@@ -126,10 +215,14 @@ CREATE TABLE `memiliki` (
 INSERT INTO `memiliki` (`id`, `review_id`, `hashtag_id`, `created_at`, `updated_at`) VALUES
 (1, 1, 1, NULL, NULL),
 (2, 2, 1, NULL, NULL),
-(3, 1, 2, NULL, NULL),
+(3, 1, 5, NULL, NULL),
 (4, 3, 1, NULL, NULL),
 (5, 4, 1, NULL, NULL),
-(6, 5, 2, NULL, NULL);
+(6, 5, 2, NULL, NULL),
+(7, 4, 3, NULL, NULL),
+(8, 3, 2, NULL, NULL),
+(9, 6, 4, NULL, NULL),
+(14, 16, 29, '2017-05-17 20:36:04', '2017-05-17 20:36:04');
 
 -- --------------------------------------------------------
 
@@ -169,24 +262,27 @@ CREATE TABLE `review` (
   `brand_id` int(11) NOT NULL,
   `kategori_id` int(11) NOT NULL,
   `judul` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `isi` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `isi` varchar(1024) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   `harga` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `toko` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `rating` int(11) NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `lihat` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `review`
 --
 
-INSERT INTO `review` (`id`, `users_id`, `brand_id`, `kategori_id`, `judul`, `isi`, `harga`, `toko`, `rating`, `created_at`, `updated_at`) VALUES
-(1, 1, 1, 1, 'test 1', 'ehehehe', '9090', 'buka tok', 3, NULL, NULL),
-(2, 2, 2, 1, 'test 2', 'ffff', '90', 'fff', 2, NULL, NULL),
-(3, 1, 2, 2, 'test 3', 'fdfvdfv', '33', 'ffff', 4, NULL, NULL),
-(4, 2, 1, 2, 'test 4', 'dfsdf', '3', 'fdf', 2, NULL, NULL),
-(5, 1, 2, 2, 'test 5', 'vfdvfdv', '4', 'dfd', 5, NULL, NULL);
+INSERT INTO `review` (`id`, `users_id`, `brand_id`, `kategori_id`, `judul`, `isi`, `harga`, `toko`, `rating`, `created_at`, `updated_at`, `lihat`) VALUES
+(1, 1, 3, 1, 'kylie', 'Estee Lauder-owned Clinique launched the concept of cosmetics being "allergy-tested," "hypoallergenic," "100% fragrance-free," and "dermatologist tested." Of those marketing claims, the only one with significance is "100% fragrance-free," which, for the most part, Clinique maintains (although it does add some fragrant extracts to a few products). Unfortunately, terms like “hypoallergenic” and “dermatologist tested” aren’t regulated by the FDA and can mean anything—thus, you still need to rely on the ingredient list to tell you whether their product contains any ingredients with the potential to irritate skin.', '123000', 'summarecon/edjkfdskfdskjfhesuk', 1, '2017-05-16 23:38:28', '2017-05-17 21:27:03', 43),
+(2, 2, 3, 3, 'bagus pol', 'That inconvenient fact aside, Clinique is leading the way with cutting-edge, state-of-the-art moisturizers and serums, plus some formidable makeup and more than a few excellent sunscreens. While Clinique has some products that we see as missteps for reasons discussed in their reviews, more than ever, what they offer is quite good (just have realistic expectations, as some of their claims go beyond what their products are capable of).', '250000', 'olshopie', 3, '2017-05-16 23:38:28', '2017-05-17 14:44:03', 7),
+(3, 3, 1, 1, 'Lipstik', 'Turning to makeup, Clinique continues to offer a vast palette of colors and textures, especially with their enormous selection of foundations—many of which feature effective sunscreens. Without a doubt, the numerous formulas offer something for every skin type and almost every skin color—though the blushes, eye makeup and lip colors are frequently not pigmented enough for deeper skin tones.', '100000', 'ulalala', 3, '2017-05-16 23:38:28', '2017-05-17 21:05:38', 31),
+(4, 2, 3, 5, 'uwaaaw', 'Clinique is categorized as one that tests on animals because their products are sold in China. Although Clinique does not conduct animal testing for their products sold elsewhere, the Chinese government requires imported cosmetics be tested on animals, so foreign companies retailing there must comply. This requirement is why some brand’s state that they don’t test on animals “unless required by law”. Animal rights organizations consider cosmetic companies retailed in China to be brands that test on animals, and so does the Beautypedia Team.', '100000', 'nyam', 2, '2017-05-16 23:38:28', '2017-05-17 14:34:59', 16),
+(5, 1, 2, 6, 'terbaik', 'This body wash''s name and claims make it sound like the antidote for dry, sensitive skin, yet its formula tells a different story. Based around the soap-like cleansing agent potassium myristate, many will find this cleanses too well, leaving skin feeling a bit dry and tight. Unlike many other cleansers that offset the potentially drying effects of potassium myristate with a high amount of glycerin or similar ingredients (or even an oil), Deep Comfort Body Wash is low on those ingredients, increasing the odds that this will have more of a drying, soap-like effect on skin.', '100000', 'nyam', 5, '2017-05-16 23:38:28', '2017-05-17 21:03:41', 12),
+(6, 3, 4, 7, 'ehehee', 'That inconvenient fact aside, Clinique is leading the way with cutting-edge, state-of-the-art moisturizers and serums, plus some formidable makeup and more than a few excellent sunscreens. While Clinique has some products that we see as missteps for reasons discussed in their reviews, more than ever, what they offer is quite good (just have realistic expectations, as some of their claims go beyond what their products are capable of).', '122000', 'www.tokopedia.com', 3, NULL, '2017-05-17 14:40:48', 4),
+(16, 3, 1, 1, 'contoh', 'coba edit hayo pin...', '123', 'wnfdjfn', 4, '2017-05-17 20:36:04', '2017-05-17 21:02:47', 6);
 
 -- --------------------------------------------------------
 
@@ -199,8 +295,8 @@ CREATE TABLE `users` (
   `nama` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `path_foto` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `aboutme` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `path_foto` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT '',
+  `aboutme` varchar(1024) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
@@ -211,8 +307,10 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `nama`, `email`, `password`, `path_foto`, `aboutme`, `remember_token`, `created_at`, `updated_at`) VALUES
-(1, 'anne annisa aulia', 'aa', '123', '/user/anne.jpg', '', NULL, NULL, NULL),
-(2, 'pina', 'pp', '123', '/user/pina.jpg', '', NULL, NULL, NULL);
+(1, 'Anne Annisa', 'c@c.c', '$2y$10$orrws6NXQGBCTpwVMwRdBeIk7CgXFYBv77hjPMXDrPK02ZKIOjwXS', '/user/anne.jpg', 'Hi semuaa! Aku mau sharing soal lippies yang lagi hits banget baik di Indonesia maupun di luar negri, cepet banget sold out nya sejak dirilis. Produk apakah itu? Yak tepat sekali, lippies yang akan aku review kali ini adalah Kylie Lip Kit.Sekarang Kylie Matte Lip Kit tersedia dalam 15 warna, sampe ada warna hitam dan biru segala, tapi aku hanya punya dua warna yaitu Koko K dan Posie K. Awalnya sempet mau beli Candy K juga tapi mengurungkan niat deh dan nyobain dua warna dulu. Yuk langsung kita mulai aja reviewnya.', NULL, NULL, NULL),
+(2, 'pina', 'p@p.p', '$2y$10$orrws6NXQGBCTpwVMwRdBeIk7CgXFYBv77hjPMXDrPK02ZKIOjwXS', '/user/pina.jpg', 'Hi semuaa! Aku mau sharing soal lippies yang lagi hits banget baik di Indonesia maupun di luar negri, cepet banget sold out nya sejak dirilis. Produk apakah itu? Yak tepat sekali, lippies yang akan aku review kali ini adalah Kylie Lip Kit.Sekarang Kylie Matte Lip Kit tersedia dalam 15 warna, sampe ada warna hitam dan biru segala, tapi aku hanya punya dua warna yaitu Koko K dan Posie K. Awalnya sempet mau beli Candy K juga tapi mengurungkan niat deh dan nyobain dua warna dulu. Yuk langsung kita mulai aja reviewnya.', NULL, NULL, NULL),
+(3, 'Tiara Amalia', 'a@a.a', '$2y$10$orrws6NXQGBCTpwVMwRdBeIk7CgXFYBv77hjPMXDrPK02ZKIOjwXS', '/user/dita.jpg', 'Hi semuaa! Aku mau sharing soal lippies yang lagi hits banget baik di Indonesia maupun di luar negri, cepet banget sold out nya sejak dirilis. Produk apakah itu? Yak tepat sekali, lippies yang akan aku review kali ini adalah Kylie Lip Kit.Sekarang Kylie Matte Lip Kit tersedia dalam 15 warna, sampe ada warna hitam dan biru segala, tapi aku hanya punya dua warna yaitu Koko K dan Posie K. Awalnya sempet mau beli Candy K juga tapi mengurungkan niat deh dan nyobain dua warna dulu. Yuk langsung kita mulai aja reviewnya.', NULL, '2017-05-17 12:49:21', '2017-05-17 14:50:49'),
+(4, 'qwe', 'qwe@qwe.qwe', '$2y$10$HNZnErRMp05jnpiIrflQY.zU4mUNWlThS9NOXoTegC5tnqgr1UjXe', '', '', NULL, '2017-05-17 21:17:34', '2017-05-17 21:17:34');
 
 --
 -- Indexes for dumped tables
@@ -274,27 +372,27 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `brand`
 --
 ALTER TABLE `brand`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 --
 -- AUTO_INCREMENT for table `foto`
 --
 ALTER TABLE `foto`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 --
 -- AUTO_INCREMENT for table `hashtag`
 --
 ALTER TABLE `hashtag`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 --
 -- AUTO_INCREMENT for table `kategori`
 --
 ALTER TABLE `kategori`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
 --
 -- AUTO_INCREMENT for table `memiliki`
 --
 ALTER TABLE `memiliki`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT for table `migrations`
 --
@@ -304,12 +402,12 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `review`
 --
 ALTER TABLE `review`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
